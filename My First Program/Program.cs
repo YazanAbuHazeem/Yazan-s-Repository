1c98f030efd2454de58cb5223ed1aa08e1a55651
Yazan Abu Hazeem <y87.abuhazeem@hotmail.com>
2018-04-09 12:18:02 +0300

    First C# Program
    This is my first C# Program

---
+++ My First Program/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_First_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Yazan", Name;
+            Name = (MyName.Equals("Yazan")) ? "Yes" : "No";
+            Console.WriteLine("{0} the name is Yazan", Name);
+            Console.ReadKey();
+        }
+    }
+}
